b3c0b0cc3cf60f89aff41cf0bab5f9bad17307f8
Wei Cheng Chen <DrChenWeiCheng@gmail.com>
2024-07-15 00:19:00 -0700

    render issue fixes

--- frontend/src/components/common/Navbar/UserInformation.jsx
+++ frontend/src/components/common/Navbar/UserInformation.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
+import "./userInfo.css";
 
-const UserInformation = () => {
+const UserInformation = ({ cookies, removeCookie }) => {
+  const [user, setUser] = useState(null); // useState at the top level
+
+  const getUserInfo = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/api/users/${cookies.auth.userid}`);
+      const data = await response.json();
+      if (data.status === 200) {
+        setUser(data.data[0]);
+        console.log(data.data[0].image);
+
+      } else {
+        console.log("Error fetching user data");
+      }
+    } catch (err) {
+      console.log("error: " + err);
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []); 
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      {user && <> 
+       <img className="userImage" src={user.image} alt="User" />
+      </>
+       }
+    </>
+  );
+};
 
-export default UserInformation
+export default UserInformation;
